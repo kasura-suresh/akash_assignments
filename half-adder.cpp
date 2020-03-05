7312293ae0ee78d129e91bf43d258dd8351cc4c9
Akash Kumar Gupta <a.k.gupta.3216@gmail.com>
2020-03-05 14:50:15 +0530

    Updated Successfully

--- half-adder.cpp
+++ half-adder.cpp
@@ -5,12 +5,10 @@ struct HalfAdder
 {
     unsigned int sum : 1;
     unsigned int carry : 1;
-};
-
-union Half{
     unsigned int A;
     unsigned int B;
 };
+
 
 unsigned int HalfAdderSum(unsigned int A,unsigned int B)
 {
@@ -25,15 +23,12 @@ unsigned int HalfAdderCarry(unsigned int A,unsigned int B)
 int main()
 {
     struct HalfAdder Half;
-    union Half H;
     cout<<"ENTER THE VALUE"<<endl;
-    cin>>H.A;
+    cin>>Half.A;
     cout<<"ENTER THE VALUE"<<endl;
-    cin>>H.B;
-    cout<<"SIZEOF STRUCTURE"<<sizeof(Half)<<endl;
-    cout<<"SIZEOF UNION"<<sizeof(H)<<endl;
-    Half.sum=HalfAdderSum(H.A,H.B);
-    Half.carry=HalfAdderCarry(H.A,H.B);
+    cin>>Half.B;
+    Half.sum=HalfAdderSum(Half.A,Half.B);
+    Half.carry=HalfAdderCarry(Half.A,Half.B);
 
     cout<<"SUM: = "<<Half.sum<<endl;
     cout<<"CARRY: = "<<Half.carry<<endl;
