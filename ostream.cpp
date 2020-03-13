37ff69b6480db595277a3107c7fd3542af9b3021
Akash Kumar Gupta <a.k.gupta.3216@gmail.com>
2020-03-13 11:54:21 +0530

    Updated Successfully

--- ostream.cpp
+++ ostream.cpp
@@ -8,12 +8,26 @@
 
 #include <iostream>
 using namespace std;
-//This class is responsible for handling output stream. It provides
-// number of function for handling chars, strings and objects such as write, put, get ..
+
+class A
+{
+    int num;
+    friend void operator>>(A& a, istream& myin)
+    {
+        myin>>a.num;
+    }
+    friend void operator<<(A& a, ostream& myout)
+    {
+        myout<<a.num;
+    }
+
+};
+
 int main()
 {
-	char ch;
-	cin.get(ch);
-	cout.put(ch);
+    A a;
+    a>>cin;
+    a<<cout;
+    return 0;
 }
 
